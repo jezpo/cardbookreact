2855b13fe65dfcff8466df4fafc3700610bfb414
jezpo <jezocam@gmail.com>
2022-06-28 15:36:50 -0400

    editing to code react not image to card

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    ///create to card in react component using storybook
+    <div className="container">
+          <div className="card-container" >
+            <div className="description">
+              <h1>Autor. Ing. Raul Gomez</h1>
+              <h5>Analisis de la investigacion sobre radio en bolivia...</h5>
+            </div>
+            <div className="header">
+              <h5>Ver Detalles</h5>
+              <a href="#">
+                <img src="book.png" alt="" width={140} height={210} />
+              </a>
+            </div>
+          </div>
+        </div>
   );
 }
 
