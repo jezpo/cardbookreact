020c3f879169c80fe54580bc48969dec45d412bd
jezpo <77103678+jezpo@users.noreply.github.com>
2022-07-03 21:57:22 +0000

    cambio de estilo usando gid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,14 +7,23 @@ function App() {
     <div className="container">
           <div className="card-container" >
             <div className="description">
-              <h1>Autor. Ing. Raul Gomez</h1>
-              <h5>Analisis de la investigacion sobre radio en bolivia...</h5>
+              <h3>Autor. Ing. Raul Gomez</h3>
+              <br/>
+              <br/>
+              <br/>
+              <div class="grid-container">
+                <div class="grid-item"><h6>Analisis de la investigacion sobre radio en bolivia...</h6></div>
+                  <div class="grid-item"><h6>325 pag</h6></div>
+                </div>
+                <br/>
+                <hr/>
             </div>
+            
             <div className="header">
               <h5>Ver Detalles</h5>
-              <a href="#">
-                <img src="book.png" alt="" width={140} height={210} />
-              </a>
+              
+              <img src="https://source.unsplash.com/random/140x210" alt="" width={140} height={210} />
+              
             </div>
           </div>
         </div>
